f88aff36b868cf8e10c68d8f68d01466e384b897
Robert Wolf <rama@ramas-Mac-mini.local>
2013-04-03 09:53:08 +1030

    Modified files commited.

--- Assets/RoarFriendsDemo.js
+++ Assets/RoarFriendsDemo.js
@@ -5,19 +5,45 @@ import Photon;
 
 private var roar:IRoar;
 
+public var LaserScope        : GameObject = null;
+public var LaserScopeBlue    : GameObject = null;
+public var LaserScopeRed     : GameObject = null;
+public var LaserScopeMagenta : GameObject = null;
+public var LaserScopeGreen   : GameObject = null;
+public var LaserScopeCyan    : GameObject = null;
+public var LaserScopeYellow  : GameObject = null;
+public var LaserScopeWhite   : GameObject = null;
+public var CurrentScope : int = 0;
+
+function findLasers () {
+	LaserScope        = gameObject.Find("WeaponSlot");
+	LaserScopeBlue    = gameObject.Find("WeaponSlotBlue");
+	LaserScopeRed     = gameObject.Find("WeaponSlotRed");
+	LaserScopeMagenta = gameObject.Find("WeaponSlotMagenta");
+	LaserScopeGreen   = gameObject.Find("WeaponSlotGreen");
+	LaserScopeCyan    = gameObject.Find("WeaponSlotCyan");
+	LaserScopeYellow  = gameObject.Find("WeaponSlotYellow");
+	LaserScopeWhite   = gameObject.Find("WeaponSlotWhite");
+}
+
 function Awake () {
 	roar = gameObject.Find("Roar").GetComponent(DefaultRoar) as IRoar;
 	if (roar == null) Debug.Log("roar is null");
 	else Debug.Log("roar found");
 	Debug.Log ("sonda");
+	findLasers();
+}
+
+function Update () {
+	if (CurrentScope < 1 || LaserScope != null) return;
+	findLasers();
+	changeLaserScopeColour(CurrentScope);
 }
 
 RoarManager.loggedInEvent += onLogin;
 
 function onLogin () {
 	Debug.Log("Just logged in.");
-	//roar.Inventory.List();
-//	roar.Properties.Fetch(fetch_callback);
 }
 
 RoarManager.propertiesReadyEvent += onPropertiesReady;
@@ -60,15 +86,99 @@ function onInventoryReady () {
 	Debug.Log("INVENTORY READY");
 	var items = roar.Inventory.List();
 	Time.timeScale = 1.0;
+	var scope_id = 1;
 	for (item in items) {
 		Debug.Log("		ITEM: [" + item.description + "] [" + item.label + "]");
 		switch (item.label) {
 		case "Super Speed": Time.timeScale += 0.5; break;
+		case "Laser Sight": scope_id += 1; break;
 		default: break;
 		}
 	}
+	changeLaserScopeColour(scope_id);
 }
 
 function onInventoryChanged () {
 	Debug.Log("INVENTORY CHANGED");
+}
+
+function changeLaserScopeColour (scope_id : int) {
+	CurrentScope = scope_id;
+	if (LaserScope == null) findLasers();
+	Debug.Log ("SCOPE COLOUR [" + scope_id + "]");
+	if (LaserScope != null) LaserScope.active = false;
+	if (LaserScopeBlue != null) LaserScopeBlue.active = false;
+	if (LaserScopeRed != null) LaserScopeRed.active = false;
+	if (LaserScopeMagenta != null) LaserScopeMagenta.active = false;
+	if (LaserScopeGreen != null) LaserScopeGreen.active = false;
+	if (LaserScopeCyan != null) LaserScopeCyan.active = false;
+	if (LaserScopeYellow != null) LaserScopeYellow.active = false;
+	if (LaserScopeWhite != null) LaserScopeWhite.active = false;
+	if (scope_id > 8) scope_id = 8;
+	switch (scope_id) {
+	case 2:
+		if (LaserScopeBlue != null) {
+			LaserScopeBlue.active = true;
+			LaserScopeBlue.GetComponent(LineRenderer).enabled = true;
+			LaserScopeBlue.GetComponent(AudioSource).enabled = true;
+			LaserScopeBlue.GetComponent(TriggerOnMouseOrJoystick).enabled = true;
+		}
+		break;
+	case 3:
+		if (LaserScopeRed != null) {
+			LaserScopeRed.active = true;
+			LaserScopeRed.GetComponent(LineRenderer).enabled = true;
+			LaserScopeRed.GetComponent(AudioSource).enabled = true;
+			LaserScopeRed.GetComponent(TriggerOnMouseOrJoystick).enabled = true;
+		}
+		break;
+	case 4:
+		if (LaserScopeMagenta != null) {
+			LaserScopeMagenta.active = true;
+			LaserScopeMagenta.GetComponent(LineRenderer).enabled = true;
+			LaserScopeMagenta.GetComponent(AudioSource).enabled = true;
+			LaserScopeMagenta.GetComponent(TriggerOnMouseOrJoystick).enabled = true;
+		}
+		break;
+	case 5:
+		if (LaserScopeGreen != null) {
+			LaserScopeGreen.active = true;
+			LaserScopeGreen.GetComponent(LineRenderer).enabled = true;
+			LaserScopeGreen.GetComponent(AudioSource).enabled = true;
+			LaserScopeGreen.GetComponent(TriggerOnMouseOrJoystick).enabled = true;
+		}
+		break;
+	case 6:
+		if (LaserScopeCyan != null) {
+			LaserScopeCyan.active = true;
+			LaserScopeCyan.GetComponent(LineRenderer).enabled = true;
+			LaserScopeCyan.GetComponent(AudioSource).enabled = true;
+			LaserScopeCyan.GetComponent(TriggerOnMouseOrJoystick).enabled = true;
+		}
+		break;
+	case 7:
+		if (LaserScopeYellow != null) {
+			LaserScopeYellow.active = true;
+			LaserScopeYellow.GetComponent(LineRenderer).enabled = true;
+			LaserScopeYellow.GetComponent(AudioSource).enabled = true;
+			LaserScopeYellow.GetComponent(TriggerOnMouseOrJoystick).enabled = true;
+		}
+		break;
+	case 8:
+		if (LaserScopeWhite != null) {
+			LaserScopeWhite.active = true;
+			LaserScopeWhite.GetComponent(LineRenderer).enabled = true;
+			LaserScopeWhite.GetComponent(AudioSource).enabled = true;
+			LaserScopeWhite.GetComponent(TriggerOnMouseOrJoystick).enabled = true;
+		}
+		break;
+	default:
+		if (LaserScope != null) {
+			LaserScope.active = true;
+			//LaserScope.GetComponent(LineRenderer).enabled = true;
+			//LaserScope.GetComponent(AudioSource).enabled = true;
+			//LaserScope.GetComponent(TriggerOnMouseOrJoystick).enabled = true;
+		}
+		break;
+	}
 }
